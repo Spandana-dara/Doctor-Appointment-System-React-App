bff1f14f7558c908ac30636d7929f7230643e455
Spandana <spandana1286@gmail.com>
2023-12-19 04:16:23 +0530

    login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import Link from "antd/es/typography/Link";
+
+const onFinishHandler = (values) => {
+  console.log(values);
+};
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h1 className="text-center">Login Form</h1>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="text" required />
+          </Form.Item>
+          <button className="btn btn-primary register-button" type="submit">
+            Login
+          </button>
+          <Link to="/register">Not a User? Register here</Link>
+        </Form>
+      </div>
+    </>
   );
 };
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Form, Input } from "antd";
+import "../styles/RegisterStyles.css";
+import Link from "antd/es/typography/Link";
+
+const onFinishHandler = (values) => {
+  console.log(values);
+};
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h1 className="text-center">Register Form</h1>
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="text" required />
+          </Form.Item>
+          <button className="btn btn-primary register-button" type="submit">
+            Register
+          </button>
+          <Link to="/login">Already User? Login here</Link>
+        </Form>
+      </div>
+    </>
   );
 };
 
